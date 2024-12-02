5a4037aad33cb0f0b3f6d0fd5a2b44a6c7ec2178
ketrab2004 <bartek@fos.nl>
2024-12-02 12:18:15 +0100

    Solved part 2 for day 2 using brute force :(

--- src/solutions/d2.rs
+++ src/solutions/d2.rs
@@ -5,7 +5,23 @@ use crate::{Input, Output};
 fn is_safe(diff: i32, i: usize, prev_diff: i32) -> bool {
     !(diff.abs() < 1
         || diff.abs() > 3
-        || (i > 1 && diff.signum() != prev_diff.signum()))
+        || (i > 1 && diff.cmp(&0) != prev_diff.cmp(&0)))
+}
+
+fn line_is_safe(line_list: impl Iterator<Item = i32>) -> bool {
+    let mut list = line_list.enumerate();
+    let mut prev = list.next().unwrap().1;
+    let mut prev_diff = 1i32;
+    for (i, num) in list {
+        let diff = num - prev;
+
+        if !is_safe(diff, i, prev_diff) {
+            return false;
+        }
+        prev = num;
+        prev_diff = diff;
+    }
+    true
 }
 
 pub fn solve(input: Input) -> Output {
@@ -17,85 +33,28 @@ pub fn solve(input: Input) -> Output {
         let line = line.unwrap();
         total += 1;
 
-        let mut list = line.split_ascii_whitespace().enumerate();
-        let mut prev = list.next().unwrap().1.parse::<i32>().unwrap();
-        let mut prev_diff = 1i32;
-        for (i, num) in list {
-            let num = num.parse::<i32>().unwrap();
-            let diff = num - prev;
+        let mut list = line
+            .split_ascii_whitespace()
+            .map(|string| string.parse::<i32>().unwrap());
 
-            if !is_safe(diff, i, prev_diff) {
-                println!("line {}:{} unsafe", total, i);
-                unsafe_count += 1;
-                break;
-            }
-            prev = num;
-            prev_diff = diff;
+        let mut list2 = list.clone();
+        let list_line = list.clone().collect::<Vec<i32>>();
+
+        if !line_is_safe(list) {
+            unsafe_count += 1;
+        } else {
+            continue;
         }
 
-        let mut skips = 0;
-        let mut i = 0usize;
-        let mut peekable_list = line.split_ascii_whitespace().peekable();
-        let mut prev = peekable_list.next().unwrap().parse::<i32>().unwrap();
-        while let Some(num) = peekable_list.next() {
-            i += 1;
-            let num = num.parse::<i32>().unwrap();
-            let diff = num - prev;
-
-            let is_unsafe = !is_safe(diff, i, prev_diff);
-            println!("{num}. diff{diff} unsafe:{is_unsafe}");
-
-            if skips <= 0 && is_unsafe {
-                // prev (first) can be skipped
-                if i <= 1 {
-                    println!("skip first i:{i} num:{num} prev:{prev}");
-                    skips += 1;
-                    if let Some(next) = peekable_list.peek() {
-                        let next = next.parse::<i32>().unwrap();
-                        let next_diff = next - num;
-                        if is_safe(next_diff, i + 1, prev_diff) {
-                            prev = num;
-                            prev_diff = diff;
-                        }
-
-                    } else {
-                        prev = num;
-                        prev_diff = diff;
-                    }
-                    i -= 1;
-                    continue;
-                }
-
-                // current can be skipped
-                if let Some(next) = peekable_list.peek() {
-                    let next = next.parse::<i32>().unwrap();
-                    let skipped_diff = next - prev;
-
-                    println!("line {}:{} skipped; skipped_diff:{skipped_diff} diff:{diff}; prev:{prev}", total, i);
-                    if is_safe(skipped_diff, i, prev_diff) {
-                        if prev != num {
-                            peekable_list.next();
-                        }
-                        skips += 1;
-                        prev_diff = skipped_diff;
-                        continue;
-                    }
-                // current (last) can be skipped
-                } else {
-                    if is_safe(prev_diff, i, prev_diff) {
-                        skips += 1;
-                        continue;
-                    }
-                }
+        if !list2
+            .enumerate()
+            .any(|(i, _num)| {
+                let mut temp = list_line.clone();
+                temp.remove(i);
+                line_is_safe(temp.iter().map(|n| *n))
             }
-
-            if is_unsafe {
-                skipped_unsafe_count += 1;
-                println!("line {}:{} unsafe; skips:{skips}; i:{i} prev_diff: {prev_diff} diff:{diff}; prev:{prev}", total, i);
-                break;
-            }
-            prev = num;
-            prev_diff = diff;
+        ) {
+            skipped_unsafe_count += 1;
         }
     }
 
