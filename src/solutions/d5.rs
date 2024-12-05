cdfc081fbfb992690b1c4305d96fac430fb82f73
ketrab2004 <bartek@fos.nl>
2024-12-05 06:54:37 +0100

    Solved day 5, both parts
    I was a little confused on what to do,
    because of that I accidentally solved part 2 before part 1

---
+++ src/solutions/d5.rs
@@ -0,0 +1,76 @@
+use std::{collections::HashMap, io::BufRead};
+use crate::{misc::option::OptionExt, Input, Output};
+
+
+fn has_incorrect_dependencies(manual: impl AsRef<[i32]>, dependencies: &HashMap<i32, Vec<i32>>) -> Option<usize> {
+    let manual = manual.as_ref();
+
+    for (i, num) in manual.iter().enumerate() {
+        let Some(dependants) = dependencies.get(num) else {
+            continue;
+        };
+
+        for dependency in dependants {
+            let Some(index) = manual.iter().position(|n| n == dependency) else {
+                continue;
+            };
+            if index < i {
+                return Some(i);
+            }
+        }
+    }
+
+    None
+}
+
+pub fn solve(input: Input) -> Output {
+    let mut dependencies = HashMap::<i32, Vec<i32>>::new();
+
+    let mut lines = input.lines();
+    for line in lines.by_ref() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+
+        let (page, dependency) = line.split_once('|').unwrap_or_err()?;
+        let page = page.parse()?;
+        let dependency = dependency.parse()?;
+
+        if let Some(dependencies) = dependencies.get_mut(&page) {
+            dependencies.push(dependency);
+        } else {
+            dependencies.insert(page, vec![dependency]);
+        }
+    }
+
+    let mut sum = 0;
+    let mut incorrect_sum = 0;
+    for line in lines {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+
+        let mut manual = line.split(',')
+            .map(|num| num.parse::<i32>())
+            .collect::<Result<Vec<_>, _>>()?;
+
+
+        if has_incorrect_dependencies(&manual, &dependencies).is_none() {
+            dbg!(&manual);
+            sum += manual.get(manual.len() / 2).unwrap_or_err()?;
+            continue;
+        }
+
+
+        while let Some(incorrect_index) = has_incorrect_dependencies(&manual, &dependencies) {
+            let removed = manual.remove(incorrect_index);
+            manual.insert(incorrect_index - 1, removed);
+        }
+
+        incorrect_sum += manual.get(manual.len() / 2).unwrap_or_err()?;
+    }
+
+    Ok((sum, incorrect_sum))
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -4,6 +4,7 @@ mod d1;
 mod d2;
 mod d3;
 mod d4;
+mod d5;
 
 
 pub fn solve_day(day: u32, input: Input) -> Option<Output> {
@@ -12,6 +13,7 @@ pub fn solve_day(day: u32, input: Input) -> Option<Output> {
         2 => Some(d2::solve(input)),
         3 => Some(d3::solve(input)),
         4 => Some(d4::solve(input)),
+        5 => Some(d5::solve(input)),
         _ => None
     }
 }
