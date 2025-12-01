694bfb466b03b82224c8aa8e9fcaa117fe09e7e2
ketrab2004 <bartek@fos.nl>
2025-12-01 06:27:13 +0100

    Day 1 20205 solutions
    pretty wasteful but it work

--- src/solutions2/d1.rs
+++ src/solutions2/d1.rs
@@ -2,14 +2,39 @@ use std::io::BufRead;
 use crate::{Input, Output, output};
 
 pub fn solve(input: Input) -> Output {
+    let mut clicks = Vec::<i32>::new();
 
     for line in input.lines() {
         let line = line?;
 
-
+        let dir = if &line[..1] == "L" {
+            -1
+        } else {
+            1
+        };
+        clicks.push(line[1..].parse::<i32>()? * dir);
     }
 
-    output!(-1)
+    let mut zero_count = 0;
+    let mut fine_zero_count = 0;
+    let mut current = 50;
+    let click_count = 100;
+
+    for click in clicks {
+        for _ in 0..(click.abs()) {
+            current = (current + click.signum()).rem_euclid(click_count);
+
+            if current == 0 {
+                fine_zero_count += 1;
+            }
+        }
+
+        if current == 0 {
+            zero_count += 1;
+        }
+    }
+
+    output!(zero_count, fine_zero_count)
 }
 
 
@@ -18,6 +43,15 @@ fn test() {
     use crate::misc::test::test_solver;
 
     test_solver(solve, indoc::indoc! {"
-
-    "}, output!(-1));
+        L68
+        L30
+        R48
+        L5
+        R60
+        L55
+        L1
+        L99
+        R14
+        L82
+    "}, output!(3, 6));
 }
